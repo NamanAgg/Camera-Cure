de075c5cb75599ce71325ae2e2828817814c718a
naman <namanagg.2010@gmail.com>
2021-07-13 20:35:59 +0530

    commit

---
+++ DBScript.js
@@ -0,0 +1,104 @@
+    let request = indexedDB.open("camera", 1);
+    let db;
+    request.onsuccess=function(e){
+        db = request.result;
+    }
+    //     let note = {
+    //         nId: "jfbuverufv",
+    //         txt: "this is naman"
+    //     }
+
+    //     let tx = db.transaction("gallery", "readwrite");
+    //     let store = tx.objectStore("gallery");
+    //     store.add(note);
+    // }
+
+    request.onerror=function(){
+        console.log("error");
+    }
+
+    request.onupgradeneeded = function (e) {
+        db = request.result;
+        db.createObjectStore("gallery", { keyPath: "nId" });
+    }
+
+function addFile(type,data) {
+    
+    let obj={
+        nId: Date.now(),
+        type: type,
+        data: data
+    }
+    let tx = db.transaction("gallery", "readwrite");
+    let store = tx.objectStore("gallery");
+    store.add(obj);
+}
+
+
+function getData() {
+    let tx = db.transaction("gallery", "readonly");
+    let store = tx.objectStore("gallery");
+    let req = store.openCursor();
+    let gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+    req.onsuccess = function (e) {
+        let cursor = req.result;
+        if (cursor) {
+            if (cursor.value.type == "image") {
+                let image = document.createElement("div");
+                image.classList.add("image");
+                image.innerHTML = `<img src='${cursor.value.data}'></img>
+                <div class="buttons">
+                    <button class="delete${cursor.value.nId}">Delete</button>
+                    <button class='download${cursor.value.nId}'>Download</button>
+                </div>`;
+                gallery.append(image);
+                let url = cursor.value.data;
+                let fileName = cursor.value.nId + ".png";
+                let nId = cursor.value.nId;
+                document.querySelector(`.download${cursor.value.nId}`).addEventListener("click", function(){
+                    download(url,fileName);
+                });
+                document.querySelector(`.delete${cursor.value.nId}`).addEventListener("click", function(){
+                    deleteFromGallery(nId);
+                });
+            } else {
+                let videoUrl = URL.createObjectURL(cursor.value.data);
+                let video = document.createElement("div");
+                video.classList.add("video");
+                video.innerHTML = `<video autoplay src='${videoUrl}' loop></video>
+                <div class="buttons">
+                    <button class="delete${cursor.value.nId}">Delete</button>
+                    <button class="download${cursor.value.nId}">Download</button>
+                </div>`;
+                gallery.append(video);
+                let fileName = cursor.value.nId + ".mp4";
+                let nId = cursor.value.nId;
+                document.querySelector(`.download${cursor.value.nId}`).addEventListener("click", function(){
+                    download(videoUrl,fileName);
+                });
+                document.querySelector(`.delete${cursor.value.nId}`).addEventListener("click", function(){
+                    deleteFromGallery(nId);
+                });
+            }
+            cursor.continue();
+        } else {
+            console.log("all data fetched");
+        }
+
+    }
+}
+
+function download(url,name) {
+    let a = document.createElement("a");
+    a.href = url;
+    a.download = name;
+    a.click();
+}
+
+function deleteFromGallery(nId) {
+    let tx = db.transaction("gallery","readwrite");
+    let store = tx.objectStore("gallery");
+    store.delete(Number(nId));
+    getData();
+}
---
+++ script.js
@@ -0,0 +1,169 @@
+///000.webbhost website for hosting it
+
+let constraints = { 
+    video: true,
+    //audio:true
+ };
+
+let videoPlayer = document.querySelector("video");
+let vidRecordBtn = document.querySelector("#record-video");
+let captureBtn = document.querySelector("#click-picture");
+let zoomIn= document.querySelector("#zoom-in");
+let zoomOut = document.querySelector("#zoom-out");
+let frame=document.querySelector(".frame");
+
+
+frame.style["max-width"]=videoPlayer.offsetWidth+"px";
+frame.style["max-height"]=videoPlayer.offsetHeight+"px";
+let zoom=1.0;
+let recordState = false;
+let chunks = [];
+let mediaRecorder;
+
+let timerInterval;
+let second = 0;
+let minute = 0;
+vidRecordBtn.addEventListener("click", function () {
+    if (!recordState) {
+        mediaRecorder.start();
+        recordState = true;
+        timerInterval = setInterval(() => {
+            second++;
+            if (second == 60) {
+                minute++;
+                second = 0;
+            }
+            if (minute < 10) {
+                document.querySelector(".minute").innerText = "0" + minute;
+            } else {
+                document.querySelector(".minute").innerText = minute;
+            }
+
+            if (second < 10) {
+                document.querySelector(".second").innerText = "0" + second;
+            } else {
+                document.querySelector(".second").innerText = second;
+            }
+
+        }, 1000);
+        vidRecordBtn.innerHTML = `<img src="https://img.icons8.com/flat-round/60/000000/stop.png"/>`;
+    } else {
+        mediaRecorder.stop();
+        recordState = false;
+        clearInterval(timerInterval);
+        second = 0;
+        minute = 0;
+        document.querySelector(".minute").innerText = "00";
+        document.querySelector(".second").innerText = "00";
+        vidRecordBtn.innerHTML = `<img src="https://img.icons8.com/flat-round/60/000000/record.png"/>`;
+    }
+});
+
+captureBtn.addEventListener("click", function () {
+    capture();
+});
+
+zoomIn.addEventListener("click",function() {
+    if (zoom <2.5) {
+        zoom += 0.1;
+        videoPlayer.style.transform = `scale(${zoom})`;
+    }
+});
+
+zoomOut.addEventListener("click", function () {
+    if(zoom!=1.0){
+        zoom-=0.1;
+        videoPlayer.style.transform= `scale(${zoom})`;
+    }
+});
+
+navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
+    videoPlayer.srcObject = mediaStream;
+    mediaRecorder = new MediaRecorder(mediaStream);
+
+    mediaRecorder.ondataavailable = function (e) {
+        chunks.push(e.data);
+    }
+
+    mediaRecorder.onstop = function () {
+        let blob = new Blob(chunks, { type: "video/mp4" });
+        chunks = [];
+        //let blobUrl = URL.createObjectURL(blob);
+        // let a = document.createElement("a");
+        // a.href = blobUrl;
+        // a.download = "temp.mp4";
+        //a.click();
+        
+        addFile("video",blob);
+    }
+})
+
+function capture() {
+    let canvas = document.createElement("canvas");
+    canvas.width = videoPlayer.videoWidth;
+    canvas.height = videoPlayer.videoHeight;
+    let ctx = canvas.getContext("2d");
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.scale(zoom, zoom);
+    ctx.translate(-(canvas.width / 2), -(canvas.height / 2));
+    ctx.drawImage(videoPlayer, 0, 0);
+    if (filter != "") {
+        ctx.fillStyle = filter;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    // let link = document.createElement("a");
+    // link.download = "img.png";
+    // link.href = canvas.toDataURL();
+    //console.log(canvas.toDataURL());
+    //link.click();
+    addFile("image",canvas.toDataURL());
+
+}
+
+let filter = "";
+
+let allFilters = document.querySelectorAll(".filter");
+
+for (let i of allFilters) {
+    i.addEventListener("click", function (e) {
+        filter = e.currentTarget.style.backgroundColor;
+        addFilterToScreen(filter);
+    })
+}
+
+function addFilterToScreen(filter) {
+    let prevScreenFilter = document.querySelector(".screen-filter");
+    if (prevScreenFilter) {
+        prevScreenFilter.remove();
+    }
+    let filterScreen = document.createElement("div");
+    filterScreen.classList.add("screen-filter");
+    filterScreen.style.height = videoPlayer.offsetHeight + "px";
+    filterScreen.style.width = videoPlayer.offsetWidth + "px";
+    filterScreen.style.backgroundColor = filter;
+    document.querySelector(".filter-screen-parent").append(filterScreen);
+}
+
+// navigator.mediaDevices.enumerateDevices().then(function (devices) {
+//     console.log(devices);
+// })
+
+
+let showGallery = document.querySelector(".show-gallery");
+
+showGallery.addEventListener("click", function (e) {
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.innerHTML = `<div class="title">
+                        <span style="margin-top: 10px; display: inline-block;">Gallery</span>
+                        <span class="close-modal" style="float: right; margin-top: 10px; margin-right: 20px; cursor: pointer;">X</span>
+                    </div>
+                    <div class="gallery">
+                    </div>`;
+    document.querySelector("body").append(modal);
+    let closeModal = document.querySelector(".close-modal");
+    closeModal.addEventListener("click", function (e) {
+        modal.remove();
+    });
+    getData();
+});
